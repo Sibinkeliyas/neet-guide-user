633c62a9767a5918b621af642ff20bd57dd1b29a
Sibin k <sibin.k@vvtsolutions.in>
2024-07-09 16:27:36 +0530

    Initial commit for the code

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import prisma from "@/lib/prisma";
+import Image from "next/image";
+import parser from "html-react-parser";
+
+export default async function Home() {
+  const data: any = await prisma.ncert_book.findFirst();
+  data["content"] = Buffer.from(data.content, "utf-8").toString();
+  return (
+    <main className=" min-h-screen p-6">
+      {parser(data.content)}
+    </main>
+  );
+}
