1dedea08cbc0743fbb0d6ef945704cf17b2e623d
Sibin k <sibin.k@vvtsolutions.in>
2024-07-09 18:50:13 +0530

    added sidebar postcaa

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,12 @@
 import prisma from "@/lib/prisma";
 import "./home.css";
 import parser from "html-react-parser";
-import ThemeToggle from "@/components/layout/ToggleTheme/theme-toggle";
 
 export default async function Home() {
   const data: any = await prisma.ncert_book.findFirst();
   data["content"] = Buffer.from(data.content, "utf-8").toString();
   return (
     <main className="min-h-screen p-6">
-      <ThemeToggle />
       {parser(data.content)}
     </main>
   );
